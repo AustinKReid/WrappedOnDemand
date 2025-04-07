4d179c5e2b5483af4a61da3c88cd411f9e87b75f
austinreid <austin.reid@roxburylatin.org>
2025-04-06 22:52:15 -0400

    SpotifyApiClient.java

--- src/main/java/com/akr/spotify/SpotifyApiClient.java
+++ src/main/java/com/akr/spotify/SpotifyApiClient.java
@@ -1,4 +1,131 @@
 package com.akr.spotify;
 
+import com.google.gson.Gson;
+import okhttp3.*;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.List;
+
 public class SpotifyApiClient {
+    private final String clientId;
+    private final String clientSecret;
+    private final String redirectUri;
+    private final OkHttpClient httpClient;
+    private final Gson gson;
+
+    private static final String AUTH_URL = "https://accounts.spotify.com/authorize";
+    private static final String TOKEN_URL = "https://accounts.spotify.com/api/token";
+    private static final String API_URL = "https://api.spotify.com/v1";
+
+    public SpotifyApiClient(String clientId, String clientSecret, String redirectUri) {
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        this.redirectUri = redirectUri;
+        this.httpClient = new OkHttpClient();
+        this.gson = new Gson();
+    }
+
+    public String getAuthorizationUrl() {
+        return AUTH_URL + "?client_id=" + clientId +
+                "&response_type=code" +
+                "&redirect_uri=" + redirectUri +
+                "&scope=user-top-read" +
+                "&show_dialog=true";
+    }
+
+    public String getAccessToken(String authorizationCode) throws IOException {
+        String credentials = clientId + ":" + clientSecret;
+        String encodedCredentials = Base64.getEncoder().encodeToString(credentials.getBytes());
+
+        RequestBody requestBody = new FormBody.Builder()
+                .add("grant_type", "authorization_code")
+                .add("code", authorizationCode)
+                .add("redirect_uri", redirectUri)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(TOKEN_URL)
+                .addHeader("Authorization", "Basic " + encodedCredentials)
+                .post(requestBody)
+                .build();
+
+        try (Response response = httpClient.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            TokenResponse tokenResponse = gson.fromJson(response.body().string(), TokenResponse.class);
+            return tokenResponse.access_token;
+        }
+    }
+
+    public List<Track> getTopTracks(String accessToken) throws IOException {
+        String url = API_URL + "/me/top/tracks?time_range=medium_term&limit=50";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("Authorization", "Bearer " + accessToken)
+                .build();
+
+        try (Response response = httpClient.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            TopTracksResponse tracksResponse = gson.fromJson(
+                    response.body().string(),
+                    TopTracksResponse.class
+            );
+            return tracksResponse.items;
+        }
+    }
+
+    // Response classes
+    private static class TokenResponse {
+        String access_token;
+        String token_type;
+        int expires_in;
+        String refresh_token;
+        String scope;
+    }
+
+    public static class TopTracksResponse {
+        List<Track> items;
+        int total;
+        int limit;
+        int offset;
+        String href;
+        String next;
+        String previous;
+    }
+
+    public static class Track {
+        public String id;
+        public String name;
+        public List<Artist> artists;
+        public Album album;
+        public int duration_ms;
+        public int popularity;
+        public String uri;
+
+        @Override
+        public String toString() {
+            return name + " by " + artists.get(0).name;
+        }
+    }
+
+    public static class Artist {
+        String id;
+        String name;
+        String uri;
+    }
+
+    public static class Album {
+        String id;
+        String name;
+        List<Image> images;
+        String uri;
+    }
+
+    public static class Image {
+        String url;
+        Integer height;
+        Integer width;
+    }
 }
